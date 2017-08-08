32eec1dfc5605b6d0c3ec472018004016d4fd30a
Dong Sup Hyeon <dhyeon@wesleyan.edu>
2017-08-08 18:22:40 -0400

    (feat) Implement basic server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var path = require('path');
+// var db = require('/db/database');
+
+var app = express();
+app.use(bodyParser.json());
+app.use('/', express.static(path.join(__dirname, '/client/index.html')));
+
+//routes
+app.get('/images', function(req, res, next) {
+  res.redirect('/')
+})
+// db.connect();
+
+var port = 4500;
+var server = app.listen(port, () => {
+  console.log('App is now running on port ', port);
+})
+
+module.exports = app;
