34901dfd659bc568462619a66ee99bb5e7a968fc
Ryan <Ryan@Toms-MacBook.local>
2017-08-09 20:23:05 -0700

    Layout for topFive component with notes for ng-repeat for topFive template.

---
+++ client/components/clientSpec/appSpec.js
@@ -0,0 +1,25 @@
+var expect = require('chai').expect;
+
+describe('app', function() {
+  it('should get five videos from the database', function() {
+    expect(/*some array that comes from db*/).to.have.a.lengthOf(5);
+  });
+
+  it('should use ng-repeat to render videos dynamically', function() {
+    expect(/*ng-repeat*/).to.exist;
+  });
+});
+
+
+// describe('app', function() {
+//   it('should list ALL blobs on /blobs GET');
+//   it('should list a SINGLE blob on /blob/<id> GET');
+//   it('should add a SINGLE blob on /blobs POST');
+//   it('should update a SINGLE blob on /blob/<id> PUT');
+//   it('should delete a SINGLE blob on /blob/<id> DELETE');
+// });
+
+// expect(foo).to.be.a('string');
+// expect(foo).to.equal('bar');
+// expect(foo).to.have.lengthOf(3);
+// expect(beverages).to.have.property('tea').with.lengthOf(3);
