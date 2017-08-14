36d084c07b18758d486500dec214fefbf1bb5de8
Dong Sup Hyeon <dhyeon@wesleyan.edu>
2017-08-14 19:09:37 -0400

    updated server spec

--- server/spec/spec.js
+++ server/spec/spec.js
@@ -2,15 +2,20 @@ var request = require('supertest');
 describe('loading express', function () {
   var server;
   beforeEach(function () {
-    server = require('./server');
+    server = require('../server');
   });
   afterEach(function () {
     server.close();
   });
-  it('responds to /', function testSlash(done) {
+  it('responds to / with success', function testSlash(done) {
   request(server)
     .get('/')
     .expect(200, done);
+  });
+  it('responds to /images with success', function testPath(done) {
+    request(server)
+      .get('/images')
+      .expect(200, done);
   });
   it('404 everything else', function testPath(done) {
     request(server)
