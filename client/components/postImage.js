0e20483e3cc09b9621ed3a959ce1287c5a4806bc
Ryan <Ryan@Toms-MacBook.local>
2017-08-16 15:38:49 -0700

    Add a postImage component.

---
+++ client/components/postImage.js
@@ -0,0 +1,11 @@
+angular.module('main')
+
+.component('postimage', {
+  controller: () => {
+    //needs information about app usage from everywhere it's being used
+    //somehow renders hot spots onto a map based on that data
+    //will need a map image that registers location
+    //something with google maps might make this doable?
+  },
+  templateUrl: '../templates/postImage.html'
+});
