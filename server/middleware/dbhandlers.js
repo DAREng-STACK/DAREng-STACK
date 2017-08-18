a79caf4f80109c0eebfca0394235f63073057fd0
Dong Sup Hyeon <dhyeon@wesleyan.edu>
2017-08-18 04:18:14 -0400

    addGeolocation method

--- server/middleware/dbhandlers.js
+++ server/middleware/dbhandlers.js
@@ -2,33 +2,27 @@ var checkUsername = (data) => {
 
 }
 
-var addGeoLocation = (data) => {
-  var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 2,
-    center: new google.maps.LatLng(2.8,-187.3),
-    mapTypeId: 'terrain'
-  });
+//addGeoLocation is a promise
+var addGeoLocation = () => {
+  // function handleLocationError() {
+  // }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      this.geoLocation = pos;
+    }, function() {
+      console.log('geolocationbroke@@@@@@@@@@@@@@@@@@@@@@@@')
 
-  // Create a <script> tag and set the USGS URL as the source.
-  var script = document.createElement('script');
-  // This example uses a local copy of the GeoJSON stored at
-  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-  script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-  document.getElementsByTagName('head')[0].appendChild(script);
-}
-
-// Loop through the results array and place a marker for each
-// set of coordinates.
-window.eqfeed_callback = function(results) {
-  for (var i = 0; i < results.features.length; i++) {
-    var coords = results.features[i].geometry.coordinates;
-    var latLng = new google.maps.LatLng(coords[1],coords[0]);
-    var marker = new google.maps.Marker({
-      position: latLng,
-      map: map
+      // handleLocationError();
     });
+  } else {
+     // Browser doesn't support Geolocation
+     console.log('geolocationbroke@@@@@@@@@@@@@@@@@@@@@@@@')
+    // handleLocationError();oke
   }
 }
-}
+
+module.exports.addGeoLocation = addGeoLocation;
